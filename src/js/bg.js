ba858e34252106bde1af02c2e7021e365169a7ea
Gürhan Mermer <gmermer@gmail.com>
2019-07-12 20:26:40 +0300

    new colours

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,16 +1,8 @@
 // Count the number of tabs
 var updateTabCount = function () {
 	chrome.tabs.query({}, function (tabs) {
-		var count = tabs.length,
-			red = count * 2,
-			blue = 200 - red;
-
-		if (red > 200) { red = 200; }
-		if (blue < 0) { blue = 0; }
-
 		chrome.browserAction.setBadgeBackgroundColor({
-			// color: [35, 85, 255, 255]
-			color: [76, 76, 220, 255]
+			color: [51, 102, 204, 255]
 		});
 
 		chrome.browserAction.setBadgeText({
